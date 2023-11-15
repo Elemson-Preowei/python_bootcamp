42e13b1ab768f302c2904c7f182f438c8bc12f4f
Elemson Preowei <elemsonp@gmail.com>
2023-11-15 21:40:28 +0100

    basic numbers file

---
+++ numbers_1.py
@@ -0,0 +1,10 @@
+numbers = [1,2,3,4,5]
+
+doubled_numbers = []
+
+for num in numbers:
+    doubled_numbers = num * 2
+print(doubled_numbers)
+    
+    #doubled_numbers.append(doubled_number)
+#print(doubled_numbers)
