6d1f8a79a4f966ddc1bdebef74c4f0096704198e
shield <myshield02@gmail.com>
2023-12-18 21:28:55 +0100

    first class on exceptions

--- error_handing.py
+++ error_handing.py
@@ -1,3 +1,25 @@
 """
 This file is going to do so many amazing stuff, all of which i'm not aware yet. LOL
 """
+
+# print('Errors in python' + 23)
+
+# for i in range(10):
+#     print(i*2)
+
+# print(friuts)
+
+# raise ValueError('value is not provided')
+# i = 0
+# while i < 10:
+#     if i % 2 == 1:
+#         raise ValueError(i, 'the value has a remainder of 1')
+#     print(i)
+#     i = i + 1
+
+def sqrt(x):
+    # if not isinstance(x, (int)):
+    #     raise TypeError('x must be numeric')
+    return x**(1/2)
+
+print(sqrt('python'))
