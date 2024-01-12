3c0e3da25fb79b356fe41dcc1c3126ca626161fd
Otuekong Etim <myshield02@gmail.com>
2024-01-12 09:58:06 +0100

    added a new way to read a file and strip line return

--- 2024-classes/file-handling.py
+++ 2024-classes/file-handling.py
@@ -19,4 +19,19 @@ with open("modules-packages.py") as fp:
     """
     This code works, the issue was that the file `modules-packages.py`
     had no content that was why we were not getting anything printed out
+
+    What happened is that there was a time we opened that file with the `w` 
+    mode, when you open an already existing file in `w` mode it will clean the
+    old content. So that was how the content I was expecting to print was lost.
+    But I have sorted it out this morning.
+    """
+with open("modules-packages.py") as fp:
+    n = 1
+    for line in fp:
+        print(n, line.rstrip("\n"))
+        n += 1
+    """
+    Here I have used the `rstrip` command to remove the return command that
+    always print a new line. 
+    You can run both and see the difference
     """
