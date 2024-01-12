2b26fc1ef2437147a9319fe1bb6461e1ddd8095a
Otuekong Etim <myshield02@gmail.com>
2024-01-12 09:57:33 +0100

    recovered lost content of the file

---
+++ 2024-classes/modules-packages.py
@@ -0,0 +1,13 @@
+"""
+# Module
+A module is simple a file in python. 
+A module can contain many functions or classes
+So this file can be a module
+
+# Package
+A package simple is a folder that contains many modules ie many .py files
+For a directory to be a package it has to contain a special
+type of file called the __init__.py
+"""
+
+from revision import sum
